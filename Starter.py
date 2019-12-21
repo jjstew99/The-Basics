c0b1794476ed71737ce6ad3fa73a481cdf203a5a
Jordan Stewart <jordanstewart@Jordans-MacBook-Air.local>
2019-12-20 19:21:09 -0500

    first commit

---
+++ Starter.py
@@ -0,0 +1,19 @@
+dog = ['ace' ', ' 'shade' ', ' 'kobe' ', ' 'bryan' ', ' 'juan' ', ' 'lacey' ', ' 'kim' ', ' 'shield' ', ' 'lion' ', ' 'bear']
+male = 'ace' ', ' 'kobe' ', ' 'bryan' ',' 'juan' ', ' 'shield' ', ' 'lion'
+female = 'shade' ', ' 'lacey' ', ' 'bear'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
